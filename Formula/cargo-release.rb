0f4c87ee34b51dc003fe95fc3315c16035dc769b
Ruoyu Zhong <zhongruoyu@outlook.com>
2023-03-13 14:54:10 +0800

    cargo-release: avoid vendored dependencies

--- Formula/cargo-release.rb
+++ Formula/cargo-release.rb
@@ -16,16 +16,50 @@ class CargoRelease < Formula
     sha256 cellar: :any_skip_relocation, x86_64_linux:   "a1beacf529d00526c4bbddfc2a099fb0d51890753ba5f5fe43d097c9ea4d7c75"
   end
 
+  depends_on "pkg-config" => :build
+  depends_on "libgit2"
+  depends_on "libssh2"
+  depends_on "openssl@1.1"
   depends_on "rust" # uses `cargo` at runtime
 
   def install
+    ENV["LIBGIT2_SYS_USE_PKG_CONFIG"] = "1"
+    ENV["LIBSSH2_SYS_USE_PKG_CONFIG"] = "1"
+    # Ensure the correct `openssl` will be picked up.
+    ENV["OPENSSL_DIR"] = Formula["openssl@1.1"].opt_prefix
+    ENV["OPENSSL_NO_VENDOR"] = "1"
+
+    # Remove the `vendored-libgit2` feature to ensure no vendoring will happen.
+    inreplace "Cargo.toml",
+      %r{git2 = { version = "(.+?)", features = \["vendored-libgit2"\], default-features = false }},
+      'git2 = { version = "\\1", features = [], default-features = false }'
+
     system "cargo", "install", *std_cargo_args
+  end
+
+  # TODO: Add this method to `brew`.
+  def check_binary_linkage(binary, library)
+    binary.dynamically_linked_libraries.any? do |dll|
+      next false unless dll.start_with?(HOMEBREW_PREFIX.to_s)
+
+      File.realpath(dll) == File.realpath(library)
+    end
   end
 
   test do
     system "cargo", "new", "hello_world", "--bin"
     cd "hello_world" do
       assert_match "tag = true", shell_output("cargo release config 2>&1").chomp
+    end
+
+    [
+      Formula["libgit2"].opt_lib/shared_library("libgit2"),
+      Formula["libssh2"].opt_lib/shared_library("libssh2"),
+      Formula["openssl@1.1"].opt_lib/shared_library("libssl"),
+      Formula["openssl@1.1"].opt_lib/shared_library("libcrypto"),
+    ].each do |library|
+      assert check_binary_linkage(bin/"cargo-release", library),
+             "No linkage with #{library.basename}! Cargo is likely using a vendored version."
     end
   end
 end
